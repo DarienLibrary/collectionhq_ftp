7420de448d4e300120740277e2957d3bfc5605e0
James McNutt <james.ryan.mcnutt@gmail.com>
2015-03-05 15:37:46 -0500

    cleaning up
    a bit cleaner code and it cleans up after itself

--- collectionhq_ftp.py
+++ collectionhq_ftp.py
@@ -1,38 +1,28 @@
 import os
-from datetime import date
-import zipfile
+from datetime import date, datetime
 from ftplib import FTP
 
-zipped = False
 host = os.environ.get('collectionhq_host')
 user = os.environ.get('collectionhq_user')
 password = os.environ.get('collectionhq_password')
 local_directory = r'C:\ProgramData\Polaris\SrServiceRoot\hermione\37\scheduledjobs'
 filename_trunc = 'bib_Collection HQ data export_{}'.format(date.today().strftime('%m%d%Y'))
+logfile = 'log_collectionhq.txt'
 filename = ''
 for f in os.listdir(local_directory):
     if f.startswith(filename_trunc): filename = f
-    
-def zip_file(filename, directory):
-    full_filename = '{}\\{}'.format(directory, filename)
-    zip_filename = filename.split('.')[0]+'.zip'
-    zf = zipfile.ZipFile(zip_filename, mode='w')
-    try:
-        zf.write(full_filename, filename)
-    finally:
-        zf.close()
-    return zip_filename
 
 def ftp_put(host, user, password, filename, directory):
-    full_filename = '{}\\{}'.format(directory, filename)
     ftp = FTP(host, user, password)
     with open(full_filename, 'rb') as f:
         ftp.storbinary('STOR {}'.format(filename), f)
+    with open(logfile, 'a') as f:
+        f.write('{}: {} uploaded from {}'.format(datetime.now(), filename, directory))
 
 if filename:
-    if zipped:
-        filename = zip_file(filename, local_directory)
-        local_directory = '.'
+    full_filename = r'{}\{}'.format(local_directory, filename)
     ftp_put(host, user, password, filename, local_directory)
-    if zipped:
-        os.remove(filename)
+    os.remove(full_filename)
+else:
+    with open(logfile, 'a') as f:
+        f.write('{}: File not found to upload'.format(datetime.now()))
